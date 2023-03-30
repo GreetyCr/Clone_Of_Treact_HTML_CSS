a33f6c33094b7b8fc75955f83e7e10aa0b92047c
GreetyCr <89504485+GreetyCr@users.noreply.github.com>
2023-03-29 19:49:45 -0600

    Added folder of FES Practice TREACT final project

---
+++ index.js
@@ -0,0 +1,9 @@
+//open menu function
+function openMenu() {
+    document.body.classList += " menu--open";
+}
+
+//close menu function
+function closeMenu() {
+    document.body.classList.remove('menu--open');
+}
